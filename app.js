ef54922020fd6a7ade72c18fa88cef2baf8af26d
Armandas G <genysarmandas@gmail.com>
2023-10-12 12:31:07 +0300

    pre-MongoDB shell

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+require('dotenv').config()
+const login = process.env.login
+
 const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = ``;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
