b967011f44773b16c86d23c9ba0425d585783a77
Armandas G <genysarmandas@gmail.com>
2023-10-17 15:10:52 +0300

    user available in views

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 require('dotenv').config()
 const secret = process.env.secret
 
@@ -22,4 +23,30 @@ else {
     res.redirect('/login');
 }
 }
-module.exports = { requireAuth };
+
+// check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, secret, async (err, decodedToken) => {
+            if(err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            }
+            else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    }
+    else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
